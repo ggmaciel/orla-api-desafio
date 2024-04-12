8d62dc297b8bde2945b28173371359c5aee21501
gustavo-de-a-maciel <gustavo.de.a.maciel@accenture.com>
2024-04-12 15:30:33 -0300

    feat: Initial commit

---
+++ src/main/java/com/ggmaciel/orlaapi/OrlaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ggmaciel.orlaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrlaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrlaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggmaciel/orlaapi/OrlaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggmaciel.orlaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrlaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
